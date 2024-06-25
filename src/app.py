db0990198b446a38aca26accba5b489f8a93f6fc
Sebastian <sebastian.gonzalez.hincapie@gmail.com>
2024-06-25 16:25:12 +0200

    EJERCICIO TERMINADO

--- src/app.py
+++ src/app.py
@@ -2,6 +2,14 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
 
 # load the .env file variables
 load_dotenv()
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
